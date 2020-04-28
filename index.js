475d353c64a717ab409c246b97c676a624358505
Jake Allison <jakeallison@Jake-Allisons-MacBook.local>
2020-04-27 17:00:09 -0700

    week 2

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     console.log(data.getAll());
-    res.render('home', {albums: data.getAll()});
+    res.render('home', {albums: JSON.stringify(data.getAll())});
 });
 
 app.get('/detail', (req,res) => {
